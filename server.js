a7d334beaa268c6da32536b9a053c24aca3ee6d7
AonMao888 <aonmao888@gmail.com>
2023-12-03 21:39:10 +0630

    5.5.7

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.get('/img/logo.png',(req,res)=>{res.sendFile(__dirname+'/assets/img/logo.png
 app.get('/css/home.css',(req,res)=>{res.sendFile(__dirname+'/assets/css/home.css')})
 app.get('/css/login.css',(req,res)=>{res.sendFile(__dirname+'/assets/css/login.css')})
 app.get('/css/signup.css',(req,res)=>{res.sendFile(__dirname+'/assets/css/signup.css')})
+app.get('/css/profile.css',(req,res)=>{res.sendFile(__dirname+'/assets/css/profile.css')})
 app.get('/font/jojar.ttf',(req,res)=>{res.sendFile(__dirname+'/assets/font/jojar.ttf')})
 app.get('/js/signup.js',(req,res)=>{res.sendFile(__dirname+'/assets/js/signup.js')})
 app.get('/page/needverify.html',(req,res)=>{res.sendFile(__dirname+'/page/needverify.html')})
@@ -50,18 +51,31 @@ app.get('/component/viewpost.ejs',(req,res)=>{res.sendFile(__dirname+'/component
 app.get('/',checkuser,async(req,res)=>{
     const { data: { user }} = await supabase.auth.getUser();
     if(user != null){
-
+        const {data:udb,err} = await supabase.from('user').select().eq('uid',user.id);
+        if(udb){
+        }else{
+            const {data,erro} = await supabase.from('user').insert({
+                uid:user.id,
+                profile_img:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+            })
+        }
     }else{
         let url = req.protocol+'://'+req.get('host')
         res.redirect(url+'/auth/login');
     }
     const {data:posts,error} = await supabase.from('post').select('*');
+    const {data:me,err} = await supabase.from('user').select().eq('uid',user.id);
     res.render('main',{
         title:"BaiMai ပၢႆးမႂ်ႇ",
         des:"Create, upload and share your memories to the world with BaiMai(ပၢႆးမႂ်ႇ)",
         component:"../component/allpost.ejs",
-        data:posts
+        data:{
+            posts:posts,
+            me:me
+        },
+        db:''
     })
+    console.log(me)
 })
 
 //listen view post page
@@ -72,9 +86,11 @@ app.get('/post/:id',async(req,res)=>{
         title:"Post | BaiMai ပၢႆးမႂ်ႇ",
         des:"What post have today..",
         component:"../component/viewpost.ejs",
-        data:data
+        data:data,
+        db:''
     })
 })
+
 //listen signup page
 app.get('/auth/signup',async(req,res)=>{
     const { data: { user } } = await supabase.auth.getUser()
@@ -146,7 +162,8 @@ app.get('/settings',async(req,res)=>{
             title:"Settings | BaiMai ပၢႆးမႂ်ႇ",
             des:"Settings for site and account.",
             component:"../component/setting.ejs",
-            data:data
+            data:data,
+            db:''
         })
     }
 })
@@ -154,28 +171,89 @@ app.get('/settings',async(req,res)=>{
 //listen upload page
 app.get('/upload',async(req,res)=>{
     let url = req.protocol+'://'+req.get('host')
-    const {data,error} = await supabase.auth.getUser();
-    if(data.user == null){
+    const {data:user,error} = await supabase.auth.getUser();
+    console.log(user)
+    if(user.user == null){
         res.redirect(url+'/auth/login?next=https://baimai.vercel.app');
     }else{
-        console.log(data)
+        const {data:udb,err} = await supabase.from('user').select().eq('uid',user.user.id);
+        console.log(udb)
         res.render('main',{
             title:"Add memories | BaiMai ပၢႆးမႂ်ႇ",
             des:"What memories you have today? Upload it to BaiMai ပၢႆးမႂ်ႇ",
             component:"../component/upload.ejs",
-            data:data
+            data:user,
+            db:udb
         })
     }
 })
 
+//listen data from upload post
+app.post('/upload',async(req,res)=>{
+    const {data,error}= await supabase.from('post').insert({
+        poster_name:req.body.uname,
+        poster_img:req.body.uimg,
+        poster_id:req.body.uid,
+        date:req.body.date,
+        des:req.body.des,
+        img1:req.body.img1,
+        img2:req.body.img2,
+        img3:req.body.img3,
+        img4:req.body.img4,
+        img5:req.body.img5,
+        time:req.body.time
+    })
+    if(data){
+        return res.redirect('../')
+    }else{
+        return res.json(error)
+    }
+})
+
+//listen logout
 app.get('/logout',async(req,res)=>{
     const {error} = await supabase.auth.signOut();
     req.session.destroy();
     res.json({status:'success'});
 })
 
-app.get('/do',(req,res)=>{
-    let url = req.protocol+'://'+req.get('host')
-    res.send(url)
+//listen profile page
+app.get('/profile/:id',async(req,res)=>{
+    const {data:profile,error} = await supabase.from('user').select().eq('uid',req.params.id);
+    const {data:{user}} = await supabase.auth.getUser();
+    if(user){
+        res.render('profile',{
+            profile:profile,
+            me:user
+        })
+        console.log(profile);
+        console.log(user)
+    }else{
+        return res.redirect('../../');
+    }
+})
+
+app.get('/api/:type/:id',async(req,res)=>{
+    let uid = req.params.id;
+    let type = req.params.type;
+    if(type == 'user'){
+        const {data,error} = await supabase.from('user').select().eq('uid',uid);
+        if(data){
+            res.json(data);
+        }else{
+            res.json({status:'failed',msg:'No user found by this id!'})
+        }
+    }else if(type == 'poster'){
+        const {data,error} = await supabase.from('user').select().eq('uid',uid);
+        if(data){
+            res.json({
+                name:data[0].name,
+                username:data[0].username,
+                profile_img:data[0].profile_img
+            });
+        }else{
+            res.json({status:'failed',msg:'No user found by this id!'})
+        }
+    }
 })
 app.listen(80,()=>console.log('server started with port 80'))
